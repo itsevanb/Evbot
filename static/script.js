e529a434758a31892693d31bf4f578d601876f10
Evan <116227653+itsevanb@users.noreply.github.com>
2023-05-18 02:30:43 -0500

    code is bit cleaner, CSS is more cleaned up
    added timestamps, animations, and fixed the spacing errors we were having

--- static/script.js
+++ static/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
     // Initialize the chatbot when the page loads
-    $('<div class="message bot-message"><i class="fas fa-robot"></i> Bot: Hello! You are chatting with a BOT.</div>').appendTo('#chatbox');
+    $('<div class="message bot-message"><i class="fas fa-robot"></i> Bot: Hello! You are chatting with a BOT.<span class="timestamp"></span></div>').appendTo('#chatbox');
 
     // Handle the Send button being clicked
-    $('#send').click(function(event) {
-        event.preventDefault();  // Prevent the form from being submitted
+    $('#send').click(sendMessage);
+
+    // Handle the Enter key in the text input
+    $('#input').on('keypress', function(e) {
+        if (e.which == 13) {  // 13 is the code for the Enter key
+            e.preventDefault();  // Prevent the default action (form submission)
+            sendMessage();  // Send the message
+        }
+    });
+
+    function sendMessage() {
         var user_input = $('#input').val();  // Get the user input
 
         // Check if the user has entered something
@@ -13,14 +22,16 @@ $(document).ready(function() {
             return;
         }
 
+        var timestamp = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true});  // Get the current time
+
         // Add the user's message to the chatbox
-        $('<div class="message user-message"><i class="fas fa-user"></i> User: ' + user_input + '</div>').appendTo('#chatbox');
+        $('<div class="message user-message"><i class="fas fa-user"></i> User: ' + user_input + '<span class="timestamp">' + timestamp + '</span></div>').appendTo('#chatbox');
 
         // Clear the input field
         $('#input').val('');
 
         // Show the bot is typing
-        var botTyping = $('<div class="message bot-message bot-typing"><i class="fas fa-robot"></i> Bot is thinking...</div>').appendTo('#chatbox');
+        var botTyping = $('<div class="message bot-message bot-typing"><i class="fas fa-robot"></i> Bot is thinking...<span class="timestamp">' + timestamp + '</span></div>').appendTo('#chatbox');
 
         // Send the user's message to the Flask backend
         $.get('/get', {msg: user_input})
@@ -28,8 +39,10 @@ $(document).ready(function() {
                 // Remove the bot is typing message
                 botTyping.remove();
 
+                var timestamp = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true});  // Get the current time
+
                 // Add the bot's response to the chatbox
-                $('<div class="message bot-message"><i class="fas fa-robot"></i> Bot: ' + data + '</div>').appendTo('#chatbox');
+                $('<div class="message bot-message"><i class="fas fa-robot"></i> Bot: ' + data + '<span class="timestamp">' + timestamp + '</span></div>').appendTo('#chatbox');
 
                 // Scroll to the bottom of the chatbox
                 $('#chatbox').scrollTop($('#chatbox')[0].scrollHeight);
@@ -41,5 +54,5 @@ $(document).ready(function() {
                 // Handle error here
                 $('<div class="message bot-message"><i class="fas fa-robot"></i> Bot: I\'m sorry, there was an error processing your request.</div>').appendTo('#chatbox');
             });
-    });
+    }
 });
